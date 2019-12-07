1388012aff463038ff10164512795a0fea779454
Burrely <dinosammy1@live.nl>
2019-12-08 00:20:00 +0100

    Basic grid rendering
    The grid will now get rendered on load and is centered in the center of the canvas.
    
    - Grid files get rendered within the canvas element on the page once the data is loaded in.
    - The grid gets an outline to define clear borders.
    - Tools window gets a scrollbar once it's getting too big for the window.
    - Grid stays centered on window resize and will be redrawn on resize too.
    - Added a bunch of utility methods within the GridHandler class to make the methods with stuff going on more readable.

--- world editing tool/assets/scripts/GridHandler.js
+++ world editing tool/assets/scripts/GridHandler.js
@@ -2,7 +2,32 @@ class GridHandler {
 
     constructor() {
         this.data;
+
+        this.elementNames = {
+            fileName: "InputFileName",
+            gridSizeX: "GridInputSizeX",
+            gridSizeY: "GridInputSizeY",
+            canvas: "RenderArea"
+        }
+        this.tileSize = 32;
+
+        this.canvas = document.getElementById(this.elementNames.canvas);
+        this.canvasContext = this.canvas.getContext("2d");
+
+        // Events
+        window.addEventListener("resize", function() { worldGrid.RenderGrid(); });
+
     }
+
+    //  Properties
+    canvasCenter() {
+        return [
+            worldGrid.canvas.width/2,
+            worldGrid.canvas.height/2
+        ];
+    }
+
+    //  Methods
 
     LoadUserGridData (file) {
         
@@ -10,17 +35,94 @@ class GridHandler {
 
     }
 
+    ResetCanvasSize() {
+        
+        worldGrid.canvas.height = worldGrid.canvas.parentElement.offsetHeight
+        worldGrid.canvas.width = worldGrid.canvas.parentElement.offsetWidth;
+
+    }
+
+    RenderOutline(coordinates = [{x:0, y:0}, {x:1, y:1}], hexColor = "#FFFFFF", width = 2 ) {
+
+        worldGrid.canvasContext.fillStyle = hexColor;
+        worldGrid.canvasContext.lineWidth = width;
+        worldGrid.canvasContext.strokeRect(
+            coordinates[0].x,
+            coordinates[0].y,
+            coordinates[1].x,
+            coordinates[1].y
+            );
+
+    }
+
+    GetRectPos(grisPosx = 0, gridPosY = 0, scale = 1, tilesX = 1, tilesY = 1) {
+
+        let canvasCenter = worldGrid.canvasCenter();
+        let gridSize = [worldGrid.data.grid.length*worldGrid.tileSize, worldGrid.data.grid[0].length*worldGrid.tileSize];
+
+        let tileOffsetFromCenter = [-(gridSize[0]/2), -(gridSize[1]/2)];
+
+        return [
+            tileOffsetFromCenter[0] + (grisPosx * worldGrid.tileSize) + canvasCenter[0],
+            tileOffsetFromCenter[1] + (gridPosY * worldGrid.tileSize) + canvasCenter[1],
+            worldGrid.tileSize * tilesX,
+            worldGrid.tileSize * tilesY
+        ];
+
+    }
+
+    RenderGrid() {
+        
+        worldGrid.ResetCanvasSize();
+        worldGrid.canvasContext.clearRect(0, 0, worldGrid.canvas.width, worldGrid.canvas.height);
+
+        for(var y=0; y < worldGrid.data.grid[0].length; y++) {
+            for(var x=0; x < worldGrid.data.grid.length; x++) {
+
+                let currentTile = worldGrid.data.grid[y][x];
+                
+                switch(currentTile.type) {
+                    case "land":
+                        worldGrid.canvasContext.fillStyle = "#00FF00";
+                    break;
+                    case "water":
+                        worldGrid.canvasContext.fillStyle = "#0000FF";
+                    break;
+                    case "wall":
+                        worldGrid.canvasContext.fillStyle = "#FF0000";
+                    break;
+                }
+
+                let tileRectPos = this.GetRectPos(x, y);
+
+                worldGrid.canvasContext.fillRect(tileRectPos[0], tileRectPos[1], tileRectPos[2], tileRectPos[3]);
+            }
+        }
+        
+        let gridOrigin = this.GetRectPos(0, 0, undefined, worldGrid.data.grid[0].length, worldGrid.data.grid.length);
+        worldGrid.RenderOutline([
+            { x: gridOrigin[0], y: gridOrigin[1]},
+            { x: gridOrigin[2], y: gridOrigin[3] }
+        ]);
+    
+    }
+
     DisplayGridData (loadedData, file) {
 
         worldGrid.data = JSON.parse(loadedData);
 
         //  Update Tools views
-        document.getElementById("InputFileName").innerHTML = file.name;
-        document.getElementById("GridInputSizeX").value = worldGrid.data.grid[0].length;
-        document.getElementById("GridInputSizeY").value = worldGrid.data.grid.length;
-
+        document.getElementById(worldGrid.elementNames.fileName).innerHTML = file.name;
+        document.getElementById(worldGrid.elementNames.gridSizeX).value = worldGrid.data.grid[0].length;
+        document.getElementById(worldGrid.elementNames.gridSizeY).value = worldGrid.data.grid.length;
+        
+        worldGrid.RenderGrid();
     }
 
 }
 
-var worldGrid = new GridHandler();
+var worldGrid;
+
+window.addEventListener("load", function() {
+    worldGrid = new GridHandler();
+});
