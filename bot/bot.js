ac974c283bc4f7a8066e176fda65c7bd4e0efa8d
Burrely <dinosammy1@live.nl>
2019-12-07 03:45:35 +0100

    Base setup bot
    - Added a config file for standard help messages
    - Added a few basic commands
    - Ordered sections of the script
    - Bot has a stop command
    - Bot will now go offline on Discord when the bot stops

--- bot/bot.js
+++ bot/bot.js
@@ -1,14 +1,68 @@
+const fileReader = require('fs');
+const helpMessages = JSON.parse( fileReader.readFileSync('HelpMessages.json') );
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+
+//  Events
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+
+  let words = msg.content.split(" ");
+
+  if (words[0] === '!poke') {
+
+    if(words[1] == undefined) {
+      Greet(msg);
+      return;
+    }
+    
+    switch(words[1]) {
+      case "stop":
+        Terminate();
+      break;
+      case "move": //WIP
+        if(words[2] == undefined) {
+          MovePlayer(msg);
+          return;
+        }
+      break;
+      case "inventory":
+        OpenInventory(msg.author, msg.channel);
+      break;
+    }
+
   }
 });
 
-client.login('NjUyNjQ0Mjg1NDA1MjAwMzk1.Xerd8Q.VYDectHCj-oaNIlC0tvatzHCTmA');
+
+//  Methods
+
+function MovePlayer(message, direction = "none") {
+  if (direction === "none") {
+    message.channel.send(helpMessages.moveHelp);
+  }
+}
+
+function OpenInventory(user, channel) {
+  channel.send("Showing inventory of " + user + "!~");
+}
+
+function Greet(message) {
+  message.channel.send("Henlo "+message.author+"!");
+}
+
+function Terminate() {
+  client.user.setStatus('invisible');
+  process.exit();
+}
+
+
+// Start
+
+client.login('NjUyNjQ0Mjg1NDA1MjAwMzk1.Xer2ig.yIWJzj1T8RAxGXsb4L8qqYNU6N4');
