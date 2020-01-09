fbcdbfe6c5b5ae98a905c97becc6a3f9c1fee81b
Burrely <dinosammy1@live.nl>
2020-01-09 14:58:43 +0100

    Basic world rendering
    Added basic world rendering to the bot's features.
    Doesn't actually load world files yet.
    
    Current features are;
    - Asynchronous response to a world render request.
    - Optimized loading of assets (only loads in the assets that are required ONCE per request).
    - Loads in only the view that the player can see. (11 by 7 tiles.
    
    Changes:
    - Added the node-canvas library for rendering images using similar code to that of the native js/html canvas. ( https://www.npmjs.com/package/canvas ).
    - Added a folder for worlds inside the bot folder.
    - Added a seperate javascript file for the game logic (logic layer).

---
+++ bot/RPGGameHandler.js
@@ -0,0 +1,152 @@
+const { createCanvas, loadImage, Image } = require('canvas');
+const path = require('path');
+const { once, EventEmitter } = require('events');
+let eventEmitter = new EventEmitter();
+
+// Settings > move to separate file later.
+
+var viewWidth = 11;
+var viewHeight = 7;
+
+const canvas = createCanvas((viewWidth*32), (viewHeight*32));
+const ctx = canvas.getContext('2d');
+
+class Instance {
+
+    constructor() {
+        
+    }
+
+    // Methods
+    async Draw(
+        world = { name: "N/A", grid: [[{texture: "grass", type: "land"}, {texture: "water", type: "land"}], [{texture: "path", type: "land"}, {texture: "grass", type: "land"}]] },
+        playerPos = {x: 1, y: 1} )
+        {
+        
+        //  Figure out view area.
+        let viewTopLeftCornerTile = {x: playerPos.x - Math.floor(viewWidth/2), y: playerPos.y - Math.floor(viewHeight/2)};
+        
+        //  Based on view area, load assets.
+        let tileAssets = new Array();
+        let assetsToLoad = new Array();
+
+        for(var y=viewTopLeftCornerTile.y; y < viewHeight; y++) {
+            for(var x=viewTopLeftCornerTile.x; x < viewWidth; x++) {
+                //  Nothing here?
+                if (world.grid[y] == undefined) {
+                    continue;
+                } else if (world.grid[x] == undefined) {
+                    continue;
+                }
+                //  Has asset already been added?
+                if (assetsToLoad.includes(world.grid[y][x].texture)) {
+                    continue;
+                }
+                //  Add asset to the tile assets list and create a promise for the asset.
+                tileAssets.push( loadImage(path.join(__dirname, `../sprites/tiles/${world.grid[y][x].texture}.png`)) );
+                //  Prevent asset from being loaded again;
+                assetsToLoad.push(world.grid[y][x].texture);
+            }
+        }
+
+        //  After all assets are loaded, render.
+        return await Promise.all(tileAssets).then((results) => {
+            return this.DrawPlayerView(world, results, assetsToLoad, viewTopLeftCornerTile);
+        }, function (error) {
+            return error;
+        });
+    }
+
+    DrawPlayerView(world, assets = new Array(new Image), assetNames = new Array(""), viewTopLeftCornerTile) {
+        
+        var y1 = 0;
+        for(var y=viewTopLeftCornerTile.y; y < viewHeight; y++) {
+            //  Nothing here?
+            if (world.grid[y] == undefined) {
+                y1++;
+                continue;
+            }
+
+            var x1 = 0;
+            for(var x=viewTopLeftCornerTile.x; x < viewWidth; x++) {
+                //  Nothing here?
+                if (world.grid[x] == undefined) {
+                    x1++;
+                    continue;
+                }
+                
+                ctx.drawImage(assets[assetNames.indexOf(world.grid[y][x].texture)], x1*32, y1*32);
+                x1++;
+            }
+
+            y1++;
+        }
+        
+        return canvas.toBuffer();
+
+    }
+
+    crap() {
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for(var y=0; y < world.length; y++) {
+            for(var x=0; x < world.grid[0].length; x++) {
+
+                let currentTile = world.grid[y][x];
+                let tileRectPos = this.GetRectPos(x, y, worldGrid.zoom);
+                
+                let image = new Image();
+
+                switch(currentTile.texture) { // WIP
+                    case "grass":
+                        if (worldGrid.sprites[0] == undefined) {
+                            image.src = "../assets/images/sprites/tiles/" + currentTile.texture + ".png";
+                            image.onload = function() {
+                                worldGrid.sprites[0] = image;
+                                worldGrid.canvasContext.drawImage(image, tileRectPos[0], tileRectPos[1], tileRectPos[2], tileRectPos[3]);
+                            }
+                        } else {
+                            image = worldGrid.sprites[0];
+                        }
+                    break;
+                    case "path":
+                        if (worldGrid.sprites[1] == undefined) {
+                            image.src = "../assets/images/sprites/tiles/" + currentTile.texture + ".png";
+                            image.onload = function() {
+                                worldGrid.sprites[1] = image;
+                                worldGrid.canvasContext.drawImage(image, tileRectPos[0], tileRectPos[1], tileRectPos[2], tileRectPos[3]);
+                            }
+                        } else {
+                            image = worldGrid.sprites[1];
+                        }
+                    break;
+                    case "water":
+                        if (worldGrid.sprites[2] == undefined) {
+                            image.src = "../assets/images/sprites/tiles/" + currentTile.texture + ".png";
+                            image.onload = function() {
+                                worldGrid.sprites[2] = image;
+                                worldGrid.canvasContext.drawImage(image, tileRectPos[0], tileRectPos[1], tileRectPos[2], tileRectPos[3]);
+                            }
+                        } else {
+                            image = worldGrid.sprites[2];
+                        }
+                    break;
+                    default:
+                        image.src = worldGrid.placeHolderImage.src;
+                    break;
+                }
+
+                worldGrid.canvasContext.drawImage(image, tileRectPos[0], tileRectPos[1], tileRectPos[2], tileRectPos[3]);
+
+            }
+
+        }
+        // ctx.font = '30px Impact';
+        // ctx.rotate(0.1);
+        // ctx.fillText('Awesome!', 50, 100);
+        // return canvas.toBuffer();
+    }
+}
+
+module.exports = Instance;
--- bot/bot.js
+++ bot/bot.js
@@ -1,11 +1,16 @@
+//
 const fileReader = require('fs');
 const path = require('path');
 
 const helpMessages = JSON.parse( fileReader.readFileSync(path.join(__dirname, 'HelpMessages.json')) );
 
+//
+const GameHandler = require('./RPGGameHandler.js');
+const game = new GameHandler();
+
+//
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 
 //  Events
 
@@ -19,11 +24,13 @@ client.on('message', msg => {
 
   if (words[0] === '!poke') {
 
+    // Base command.
     if(words[1] == undefined) {
       Greet(msg);
       return;
     }
     
+    // Commands
     switch(words[1]) {
       case "stop":
         Terminate();
@@ -37,6 +44,10 @@ client.on('message', msg => {
       case "inventory":
         OpenInventory(msg.author, msg.channel);
       break;
+      case "draw":
+        //msg.channel.sendFile(game.Draw());
+        Draw(msg);
+      break;
     }
 
   }
@@ -44,6 +55,17 @@ client.on('message', msg => {
 
 
 //  Methods
+async function Draw(msg) {
+
+  let renderPromise = new Promise((resolve, reject) => {
+    resolve(game.Draw());
+  });
+  renderPromise.then((value) => {
+    let render = new Discord.Attachment(value, "GameState.png");
+    msg.channel.send(render);
+  });
+
+}
 
 function MovePlayer(message, direction = "none") {
   if (direction === "none") {
@@ -67,4 +89,4 @@ function Terminate() {
 
 // Start
 
-client.login('NjY0NDEyMDY0MTE4MjEwNTcx.XhWs5A.zfOpEF_G6j18PT-3zHSDLyEI1RY');
+client.login('NjY0NDEyMDY0MTE4MjEwNTcx.XhXTEA.845qtVq_ig8fBjM11OVJpIUWiGc');
